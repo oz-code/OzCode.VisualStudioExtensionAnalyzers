7305cf3fa122507a05d7a72486c2e7ae6c3b0654
Michael Shpilt <michaels@codevalue.net>
2017-05-11 15:35:13 +0300

    Changed name and description of the project
    Removed test project from solution

--- MefImportExceptionAnalyzer/MefImportExceptionAnalyzer/Resources.Designer.cs
+++ MefImportExceptionAnalyzer/MefImportExceptionAnalyzer/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace OzCode.Analyzers.AddTryCatchAnalyzer {
+namespace OzCode.VisualStudioExtensionAnalyzers {
     using System;
     using System.Reflection;
     
@@ -40,7 +40,7 @@ namespace OzCode.Analyzers.AddTryCatchAnalyzer {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("OzCode.Analyzers.AddTryCatchAnalyzer.Resources", typeof(Resources).GetTypeInfo().Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("OzCode.VisualStudioExtensionAnalyzers.Resources", typeof(Resources).GetTypeInfo().Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
